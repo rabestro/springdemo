e24108489c1d58648ad7b357cc7d5561890b04d5
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-15 18:38:32 +0300

    Added ClassicalMusic

---
+++ src/main/java/com/github/rabestro/springdemo/ClassicalMusic.java
@@ -0,0 +1,11 @@
+package com.github.rabestro.springdemo;
+
+/**
+ * @author Neil Alishev
+ */
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
