be993c2c03842a89638133787b34a176c66ff8c2
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-15 18:39:30 +0300

    Added RockMusic

---
+++ src/main/java/com/github/rabestro/springdemo/RockMusic.java
@@ -0,0 +1,11 @@
+package com.github.rabestro.springdemo;
+
+/**
+ * @author Neil Alishev
+ */
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Wind cries Mary";
+    }
+}
