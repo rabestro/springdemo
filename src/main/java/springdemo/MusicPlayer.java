d6d582c820c3bb6c96f5d798d7f009982229da24
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-21 18:47:55 +0300

    Added setter for class MusicPlayer

--- src/main/java/springdemo/MusicPlayer.java
+++ src/main/java/springdemo/MusicPlayer.java
@@ -6,8 +6,19 @@ package springdemo;
 public class MusicPlayer {
     private Music music;
 
+    public MusicPlayer() {
+    }
+
     // IoC
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
