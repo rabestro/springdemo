eb4d8b8598274fef8e50719abeba865998f4234c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-21 22:42:23 +0300

    Added init method to Jazz class

--- src/main/java/springdemo/Application.java
+++ src/main/java/springdemo/Application.java
@@ -3,25 +3,14 @@ package springdemo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static java.text.MessageFormat.format;
-
 public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
         final var context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        final var userBean = context.getBean("userBean", UserBean.class);
-        System.out.println(format("Hello, {0}!", userBean.getName()));
-
         final var musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        final var otherPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        otherPlayer.setVolume(30);
 
         musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
-        System.out.println(otherPlayer.getVolume());
         context.close();
     }
 }
