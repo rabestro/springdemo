f6b85259ad2c8954d988082bac770f3980cc79f7
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-25 17:33:10 +0300

    Added bean bundleSongs

--- src/main/java/springcourse/SpringConfig.java
+++ src/main/java/springcourse/SpringConfig.java
@@ -1,7 +1,15 @@
 package springcourse;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ResourceBundle;
 
 @Configuration
 public class SpringConfig {
+
+    @Bean
+    ResourceBundle bundleSongs() {
+        return ResourceBundle.getBundle("songs");
+    }
 }
